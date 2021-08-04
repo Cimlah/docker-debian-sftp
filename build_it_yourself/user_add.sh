b0b77974bce177e20a977d12f4642de58ee52a9a
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-08-04 14:41:16 +0200

    Improved files permissions, so they (should) work properly

--- build_it_yourself/user_add.sh
+++ build_it_yourself/user_add.sh
@@ -8,4 +8,6 @@ read password
 
 useradd $user -s /usr/sbin/nologin -g sftp-users -m && \
 echo $user:$password | chpasswd && \
-chmod 700 /home/$user && setfacl -dRm u::7,g::0,o::0 /home/$user
+chmod 700 /home/$user && setfacl -dRm u::7,g::0,o::0 /home/$user && mkdir -p /home/$user/$user-files && \
+chown -R $user:sftp-users /home/$user/$user-files/ && setfacl -dRm u::7,g::0,o::0 /home/$user/$user-files && \
+chown 755 /home/$user/
