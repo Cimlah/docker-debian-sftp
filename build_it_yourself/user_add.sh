b72fa79a0871213f46f57dd7b4594646212d6f29
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-08-04 14:59:12 +0200

    Improved permissions in user_add.sh

--- build_it_yourself/user_add.sh
+++ build_it_yourself/user_add.sh
@@ -10,4 +10,4 @@ useradd $user -s /usr/sbin/nologin -g sftp-users -m && \
 echo $user:$password | chpasswd && \
 chmod 700 /home/$user && setfacl -dRm u::7,g::0,o::0 /home/$user && mkdir -p /home/$user/$user-files && \
 chown -R $user:sftp-users /home/$user/$user-files/ && setfacl -dRm u::7,g::0,o::0 /home/$user/$user-files && \
-chown root /home/$user/
+chown root /home/$user/ && chmod 755 /home/$user
